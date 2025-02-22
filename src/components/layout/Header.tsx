1cd832f6140acf0e124f8883d3723813774aa936
Shivanshu pal <palshivanshu2005@gmail.com>
2025-02-22 14:37:51 +0530

    changed the landing page bgc

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,7 +24,7 @@ export function Header() {
 
           <div className="hidden md:flex flex-1 max-w-xl mx-30">
             <div className="relative w-full">
-              <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+              <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-white" />
               <input
                 type="search"
                 placeholder="Search"
